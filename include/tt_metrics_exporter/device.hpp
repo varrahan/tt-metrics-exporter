4b2ac054a34acdab1445a03b7974f0623d8f5f5e
varrahan <varrahan@gmail.com>
2026-07-09 23:41:28 -0400

    Update device header file to provide more telemetry data

--- include/tt_metrics_exporter/device.hpp
+++ include/tt_metrics_exporter/device.hpp
@@ -21,6 +21,8 @@ struct CharacterDevice {
 };
 
 struct PciInfo {
+  std::optional<std::string> bdf;
+  std::optional<std::string> driver;
   std::optional<std::string> vendor_id;
   std::optional<std::string> device_id;
   std::optional<std::string> class_id;
@@ -28,6 +30,12 @@ struct PciInfo {
   std::optional<std::string> subsystem_vendor_id;
   std::optional<std::string> subsystem_device_id;
   std::optional<std::int64_t> numa_node;
+  std::optional<std::int64_t> iommu_group;
+  std::optional<std::string> current_link_speed;
+  std::optional<std::int64_t> current_link_width;
+  std::optional<std::string> max_link_speed;
+  std::optional<std::int64_t> max_link_width;
+  std::optional<std::string> reset_method;
 };
 
 struct PciResource {
@@ -78,13 +86,78 @@ struct HwmonSensor {
   std::int64_t value = 0;
 };
 
-struct DeviceSpec {
-  std::string card_type;
-  std::int64_t asic_count = 0;
-  std::int64_t tensix_cores = 0;
-  std::int64_t sram_bytes = 0;
-  std::int64_t memory_bytes = 0;
-  std::int64_t memory_bandwidth_bytes_per_second = 0;
+struct MemoryTelemetry {
+  std::optional<std::uint64_t> used_bytes;
+  std::optional<std::uint64_t> total_bytes;
+  std::optional<std::uint64_t> free_bytes;
+  std::optional<std::uint64_t> available_bytes;
+  std::optional<std::uint64_t> bandwidth_bytes_per_second;
+  std::optional<std::string> type;
+  std::optional<std::string> controller_layout;
+  std::optional<std::int64_t> controller_count;
+  std::optional<std::int64_t> controllers_per_asic;
+  std::optional<std::int64_t> channel_count;
+};
+
+struct TensixTelemetry {
+  std::optional<std::int64_t> used;
+  std::optional<std::int64_t> available;
+  std::optional<std::int64_t> total;
+  std::optional<std::int64_t> mesh_rows;
+  std::optional<std::int64_t> mesh_cols;
+  std::optional<std::string> topology;
+  std::optional<std::string> active_regions;
+  std::optional<std::string> source;
+};
+
+struct MetaliumWorkloadTelemetry {
+  std::string workload_id;
+  std::optional<std::string> pod_namespace;
+  std::optional<std::string> pod_name;
+  std::optional<std::string> container_name;
+  std::optional<std::int64_t> active;
+  std::optional<std::int64_t> programs_observed;
+  std::optional<std::int64_t> tensix_cores_used;
+  std::optional<std::int64_t> tensix_cores_total;
+  std::optional<std::int64_t> sample_timestamp_seconds;
+  bool stale = false;
+};
+
+struct HealthTelemetry {
+  std::optional<std::string> fault_code;
+  std::optional<std::string> fault_reason;
+  std::optional<std::int64_t> reset_required;
+  std::optional<std::int64_t> oom_fault_count;
+  std::optional<std::int64_t> hang_fault_count;
+};
+
+struct InterconnectLink {
+  std::string name;
+  std::optional<std::string> type;
+  std::optional<std::string> state;
+  std::optional<std::string> peer;
+  std::optional<std::int64_t> speed_gbps;
+  std::optional<std::string> ring_id;
+};
+
+struct AllocationTelemetry {
+  std::optional<std::string> claim_namespace;
+  std::optional<std::string> claim_name;
+  std::optional<std::string> claim_uid;
+  std::optional<std::string> pod_namespace;
+  std::optional<std::string> pod_name;
+  std::optional<std::string> container_name;
+};
+
+struct JanitorTelemetry {
+  std::optional<std::string> state;
+  std::optional<std::string> quarantine_reason;
+  std::optional<std::string> last_scrub_status;
+  std::optional<std::string> last_reset_status;
+  std::optional<std::int64_t> scrub_count;
+  std::optional<std::int64_t> reset_count;
+  std::optional<std::int64_t> last_scrub_timestamp_seconds;
+  std::optional<std::int64_t> last_reset_timestamp_seconds;
 };
 
 struct DeviceTelemetry {
@@ -100,7 +173,13 @@ struct DeviceTelemetry {
   std::vector<NamedCounter> pcie_counters;
   FirmwareTelemetry firmware;
   std::vector<HwmonSensor> hwmon_sensors;
-  std::optional<DeviceSpec> spec;
+  MemoryTelemetry memory;
+  TensixTelemetry tensix;
+  std::vector<MetaliumWorkloadTelemetry> metalium_workloads;
+  HealthTelemetry health_detail;
+  std::vector<InterconnectLink> interconnect_links;
+  AllocationTelemetry allocation;
+  JanitorTelemetry janitor;
   std::optional<std::uint64_t> memory_used_bytes;
   std::optional<std::uint64_t> memory_total_bytes;
   std::optional<std::int64_t> tensix_cores_used;
@@ -109,6 +188,10 @@ struct DeviceTelemetry {
 
 struct CollectorConfig {
   std::filesystem::path sysfs_root = "/sys/class/tenstorrent";
+  std::filesystem::path allocation_state_root;
+  std::filesystem::path janitor_state_root;
+  std::filesystem::path metalium_profiler_state_root;
+  std::int64_t metalium_profiler_stale_after_seconds = 15;
   bool collect_pcie_counters = false;
 };
 
